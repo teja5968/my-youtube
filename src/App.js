3a1133336cbe21f21270da166458842dc4055212
teja5967 <tejamaroju123@gmail.com>
2024-12-10 08:59:08 +0530

    created header and sidebar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@
 import './App.css';
 import Body from './components/Body';
 import Header from './components/Header';
+import { Provider } from 'react-redux';
+
+import store from './utils/store';
 
 function App() {
   return (
+
+
+    <Provider store = {store} >
     <div className="App">
       {/* <h1 className="text-green-700 font-bold">Ganesha</h1> */}
 
@@ -23,11 +29,14 @@ function App() {
       
       */}
 
+
+
       <Header />
       <Body />
 
     
     </div>
+    </Provider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,38 @@
 import React from 'react'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+
 
 const Header = () => {
+
+
+ 
   return (
-    <div className='grid grid-flow-col '>
+    <div className='grid grid-flow-col p-5 m-2 shadow-lg '>
 
-        <div className='flex'>
+        <div className='flex col-span-1'>
 
-        <img className ='h-9 object-contain' src = "https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/What%20is%20a%20Hamburger%20Button.png?width=225&name=What%20is%20a%20Hamburger%20Button.png" alt = "menu"/>
-        <img  className = "w-30 h-10 object-contain" src = "https://cdn.mos.cms.futurecdn.net/8gzcr6RpGStvZFA2qRt4v6-1200-80.jpg" alt ="youtube"/>
+        <img className ='h-8' src = "https://icons.veryicon.com/png/o/miscellaneous/linear-icon-45/hamburger-menu-4.png" alt = "hamburger menu"/>
+        <img  className = " mx-2 h-7 object-contain " src = "https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg" alt ="youtube"/>
+        </div>
+
+        <div className='col-span-10 px-10 flex'>
+            <input  className = "w-1/2 border border-gray-400 rounded-l-full px-5 py-2 " type = "text" placeholder='Search'/>
+            <button className="border border-gray-400 p-2 rounded-r-full  bg-gray-100">
+           <MagnifyingGlassIcon className="h-6 w-5" />
+</button>
         </div>
 
         <div>
-            <input type = "text" placeholder='Search'/>
-            <button>Search</button>
-        </div>
-
-        <div>
-            <img className = " h-8 " src = "https://cdn-icons-png.flaticon.com/512/1144/1144760.png" alt ="user-icon"/>
+          
+           
+  <img className = " h-9" src="https://cdn-icons-png.flaticon.com/512/847/847969.png" alt="User Avatar"/>
+ 
         </div>
     </div>
+
+
+ 
+
   )
 }
 
